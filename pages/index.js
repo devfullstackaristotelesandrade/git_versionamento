46d6a3ccc68282c058894148049ed3ae9a68632a
devfullstackaristotelesandrade <dev.full.stack.aristotelesandrade@gmail.com>
2026-03-08 21:42:48 +0000

    adicionar scripts`lint:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
-function Home(){
-    return <h1> Perseverança é alma para programar</h1>
+function Home() {
+  return <h1> Perseverança é alma para programar</h1>;
 }
 
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("indentação errada");
+}
 
 export default Home;
